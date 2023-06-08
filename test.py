12561df9a5f7152d9638b26e5809067509487073
vlad <vlad5123ful@gmail.com>
2023-06-09 01:48:19 +0300

    pip8 standart fix

--- test.py
+++ test.py
@@ -1,34 +1,35 @@
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
-
-y_S = np.zeros((4,1),np.int)
-counter = 0 
-test_y=2000
 
 
-img1= cv2.imread('C:/Users/HOME/Desktop/mmro/1/DSC02694.jpg',0)
-img2= cv2.imread('C:/Users/HOME/Desktop/mmro/1/DSC02692.jpg',0)
+y_S = np.zeros((4, 1), np.int)
+counter = 0
+test_y = 2000
+
+
+img1 = cv2.imread('C:/Users/HOME/Desktop/mmro/1/DSC02694.jpg', 0)
+img2 = cv2.imread('C:/Users/HOME/Desktop/mmro/1/DSC02692.jpg', 0)
 img1 = cv2.resize(img1, (1920, 1080))
 img2 = cv2.resize(img2, (1920, 1080))
-
 sift = cv2.SIFT_create()
+
 
 kp1, des1 = sift.detectAndCompute(img1, None)
 kp2, des2 = sift.detectAndCompute(img2, None)
 
 
-
 FLANN_INDEX_KDTREE = 1
 index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
-search_params = dict(checks=50) 
+search_params = dict(checks=50)
 flann = cv2.FlannBasedMatcher(index_params, search_params)
 matches = flann.knnMatch(des1, des2, k=2)
+
 
 matchesMask = [[0, 0] for i in range(len(matches))]
 good = []
 pts1 = []
 pts2 = []
+
 
 for i, (m, n) in enumerate(matches):
     if m.distance < 0.7*n.distance:
@@ -36,7 +37,6 @@ for i, (m, n) in enumerate(matches):
         good.append(m)
         pts2.append(kp2[m.trainIdx].pt)
         pts1.append(kp1[m.queryIdx].pt)
-
 
 
 pts1 = np.int32(pts1)
@@ -54,12 +54,9 @@ _, H1, H2 = cv2.stereoRectifyUncalibrated(
 )
 
 
-
 img1_rectified = cv2.warpPerspective(img1, H1, (w1, h1))
 img2_rectified = cv2.warpPerspective(img2, H2, (w2, h2))
 
-cv2.imshow('image2',img1_rectified)
-cv2.imshow('image2',img2_rectified)
+cv2.imshow('image2', img1_rectified)
+cv2.imshow('image2', img2_rectified)
 cv2.waitKey(1)
-
-
